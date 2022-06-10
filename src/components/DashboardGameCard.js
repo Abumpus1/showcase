db2604fd7353d2b5eb530694cc7aad58a1a2d5fa
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-06-09 19:32:54 -0600

    Add DashboardGameCard

---
+++ src/components/DashboardGameCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/DashboardGameCard.css";
+
+function DashboardGameCard() {
+  return (
+    <div className="dash-game-card">
+      GAME CARD BUHBAM
+    </div>
+  )
+}
+
+export default DashboardGameCard;
--- src/components/StoreDealBox.js
+++ src/components/StoreDealBox.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/StoreDealBox.css";
+import DashboardGameCard from "./DashboardGameCard";
 
 function StoreDealBox() {
   return (
     <div className="store-deal-box">
       Hello Deal Box
+      <DashboardGameCard />
+      <DashboardGameCard />
+      <DashboardGameCard />
+      <DashboardGameCard />
+      <DashboardGameCard />
     </div>
   )
 }
