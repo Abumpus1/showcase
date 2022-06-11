87b2830d264ba68bd3fc238528fd86e032726bc0
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-06-11 14:06:18 -0600

    Allow user to redirect to game on click

--- src/components/DashboardGameCard.js
+++ src/components/DashboardGameCard.js
@@ -5,7 +5,11 @@ function DashboardGameCard({ game }) {
   console.log(game);
   return (
     <div className="dash-game-card">
-      <h4 className="dash-game-title">{game.title}</h4>
+
+        <a href={`https://www.cheapshark.com/redirect?dealID=${game.dealID}`} target="_blank">
+          <h4 title={game.title} className="dash-game-title">{game.title}</h4>
+        </a>
+
       <div className="dash-price-box">
         <p className="dash-old-price">${game.normalPrice}</p>
         <p className="dash-sale-price">${game.salePrice}</p>
