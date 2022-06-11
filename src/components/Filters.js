d9351bb897239920d4dbfafa86d02b712dfca0ea
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-06-11 17:57:05 -0600

    Add Price limit slider, adjust css

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Filters.css";
 
 function Filters() {
 
   const [searchInput, setSearchInput] = useState("")
+  const [maxPrice, setMaxPrice] = useState("50")
+
+  const displayMaxPrice = () => {
+    return parseInt(maxPrice) < 50 ? `$${maxPrice} and under` : "No limit"
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
 
   return (
-    <form className="filters">
-      <div className="title-box">
-        <label className="title-search-label" for="title-search">TITLE:</label>
-        <input value={searchInput} onChange={(event) => setSearchInput(event.target.value)} name="title-search" placeholder="Search By Title..." className="title-search" type="text"/>
+    <form onSubmit={event => handleSubmit(event)} className="filters">
+      <div className="filter-box">
+        <div className="title-box">
+          <label className="title-search-label" htmlFor="title-search">TITLE:</label>
+          <input value={searchInput} onChange={event => setSearchInput(event.target.value)} name="title-search" placeholder="Search By Title..." className="title-search" type="text"/>
+        </div>
+        <div className="price-limit-box">
+          <p>Price Limit: {displayMaxPrice()}</p>
+          <input type="range" min="1" max="50" value={maxPrice} onChange={event => setMaxPrice(event.target.value)}/>
+        </div>
       </div>
+      <button className="apply-filters-btn">APPLY</button>
     </form>
   )
 }
