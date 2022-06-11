a2cf7a90469e843055ce6b980c3ef84a1d30467c
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-06-11 15:33:01 -0600

    Add search input

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Filters.css";
 
 function Filters() {
+
+  const [searchInput, setSearchInput] = useState("")
+
   return (
-    <div className="filters">
-      HELLO I AM FILTERS
-    </div>
+    <form className="filters">
+      <div className="title-box">
+        <label className="title-search-label" for="title-search">TITLE:</label>
+        <input value={searchInput} onChange={(event) => setSearchInput(event.target.value)} name="title-search" placeholder="Search By Title..." className="title-search" type="text"/>
+      </div>
+    </form>
   )
 }
 
