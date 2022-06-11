a05be4670e54d204220c1bd9e862eee213f6b891
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-06-11 14:27:01 -0600

    Clean files

--- src/components/DashboardGameCard.js
+++ src/components/DashboardGameCard.js
@@ -5,11 +5,9 @@ function DashboardGameCard({ game }) {
   console.log(game);
   return (
     <div className="dash-game-card">
-
         <a href={`https://www.cheapshark.com/redirect?dealID=${game.dealID}`} target="_blank">
           <h4 title={game.title} className="dash-game-title">{game.title}</h4>
         </a>
-
       <div className="dash-price-box">
         <p className="dash-old-price">${game.normalPrice}</p>
         <p className="dash-sale-price">${game.salePrice}</p>
