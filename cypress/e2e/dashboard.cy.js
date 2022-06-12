121ad24cec1f40264725d5d8d0b76d957f0ea8f6
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-06-12 14:24:07 -0600

    Stub dashboard requests

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -1,6 +1,16 @@
 describe('Dashboard', () => {
   beforeEach(() => {
+    cy.intercept("GET", "https://www.cheapshark.com/api/1.0/stores", { fixture: "allStores.json" })
+
+    cy.intercept("GET", "https://www.cheapshark.com/api/1.0/deals?storeID=1&pageSize=5", { fixture: "store1.json" })
+    cy.intercept("GET", "https://www.cheapshark.com/api/1.0/deals?storeID=7&pageSize=5", { fixture: "store7.json" })
+    cy.intercept("GET", "https://www.cheapshark.com/api/1.0/deals?storeID=8&pageSize=5", { fixture: "store8.json" })
+    cy.intercept("GET", "https://www.cheapshark.com/api/1.0/deals?storeID=11&pageSize=5", { fixture: "store11.json" })
+    cy.intercept("GET", "https://www.cheapshark.com/api/1.0/deals?storeID=25&pageSize=5", { fixture: "store25.json" })
+    cy.intercept("GET", "https://www.cheapshark.com/api/1.0/deals?storeID=27&pageSize=5", { fixture: "store27.json" })
+    
     cy.visit('http://localhost:3000/')
+
   })
 
   it('User should be able to view dashboard', () => {
--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,5 +1,10 @@
 describe('Search', () => {
-  it('passes', () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://www.cheapshark.com/api/1.0/stores", { fixture: "allStores.json" })
+    
     cy.visit('http://localhost:3000/search')
+  })
+
+  it('passes', () => {
   })
 })
