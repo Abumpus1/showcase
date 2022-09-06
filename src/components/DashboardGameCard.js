52d29e35c6c763a2559018bcfd9e2a136067af38
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-09-06 15:36:08 -0600

    Adjust links to allow user to click entire box instead of title

--- src/components/DashboardGameCard.js
+++ src/components/DashboardGameCard.js
@@ -4,15 +4,15 @@ import PropTypes from "prop-types";
 
 function DashboardGameCard({ title, dealID, normalPrice, salePrice }) {
   return (
-    <div className="dash-game-card">
-        <a href={`https://www.cheapshark.com/redirect?dealID=${dealID}`} target="_blank">
-          <h4 title={title} className="dash-game-title">{title}</h4>
-        </a>
+    <a href={`https://www.cheapshark.com/redirect?dealID=${dealID}`} target="_blank" className="dash-game-card">
+      <div href={`https://www.cheapshark.com/redirect?dealID=${dealID}`} target="_blank">
+        <h4 title={title} className="dash-game-title">{title}</h4>
+      </div>
       <div className="dash-price-box">
         <p className="dash-old-price">${normalPrice}</p>
         <p className="dash-sale-price">${salePrice}</p>
       </div>
-    </div>
+    </a>
   )
 }
 
